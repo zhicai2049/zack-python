276305de927871adcc8038177014f9ac1ea79ca5
Zhicai <1195061622@qq.com>
2022-04-03 15:27:52 +0800

    add func

---
+++ notes/tutorial_notes.md
@@ -0,0 +1,13 @@
+### 概览
+1. 拥有高级数据结构，支持面向对象编程
+2. 动态类型，解释语言，适合脚本及通用程序开发
+3. 广泛的标准库和第三方库， https://www.python.org/
+4. 能使用C/C++语言对Python解释器进行扩展，完成定制功能
+
+### 术语
+1. abstract base class ： 抽象基类，完成很多基础性工作，详情看abc模块
+
+### 其他
+1. 执行脚本  python -m module [arg] ...
+2. UNIX "shebang" line:  #!/usr/bin/env python3
+3. 指定编码：# -*- coding: encoding -*-
---
+++ src/funcs/zack_func.py
@@ -0,0 +1,10 @@
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- This parrot wouldn't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- Lovely plumage, the", type)
+    print("-- It's", state, "!")
+
+def parrot(voltage, state='a stiff'):
+    print("if you put", voltage, "volts through it.")
+    print("-- It's", state, "!")
+
